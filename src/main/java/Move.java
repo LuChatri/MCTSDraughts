60b967b7a8ca9a6d635f2d90f8df0e9f3fdbabca
CluelessMe <ldcmiga@gmail.com>
2021-06-24 14:55:28 -0600

    Document and simplify Move

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -1,45 +1,66 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents actions taken in one turn of draughts.
  */
 public class Move {
 
-    private int start;
-    private List<Integer> offsets;
+    private final int start;
+    private final List<Offset> offsets;
 
-    public Move() {
-
+    /**
+     * Constructs a move.
+     *
+     * A Move has two components: a start and a series of offsets. The start
+     * is the square upon which the moving piece began its turn. Squares are
+     * numbered from top left to bottom right <b>starting at 1 and excluding
+     * the 32 squares not used in draughts.</b>. Offsets are actions (jumps
+     * or normal moves) taken sequentially from the start square.
+     *
+     * @param start The square (1-32) that the moving piece started on.
+     * @param offsets Actions taken by the moving piece.
+     */
+    public Move(int start, List<Offset> offsets) {
+        this.start = start;
+        this.offsets = new ArrayList<>(offsets);
     }
 
-    public Move(int start, List<Integer> offsets) {
-
-    }
-
-    public Move(int start, int... offsets) {
-
-    }
-
-    public void makeMove(GameState state) {
-
+    /**
+     * @see #Move(int, List)
+     */
+    public Move(int start, Offset... offsets) {
+        this(start, Arrays.asList(offsets));
     }
 
     public int getStart() {
-        return 0;
+        return start;
     }
 
-    public List<Integer> getOffsets() {
-        return new ArrayList<>();
+    public List<Offset> getOffsets() {
+        return offsets;
     }
 
     @Override
     public String toString() {
-        return "";
+        StringBuilder builder = new StringBuilder("Move: ");
+        builder.append(start);
+        for (Offset o : getOffsets()) {
+            builder.append("-->");
+            builder.append(o.toString());
+        }
+        return builder.toString();
     }
 
     @Override
     public boolean equals(Object o) {
-        return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Move move = (Move) o;
+        return start == move.start && Objects.equals(offsets, move.offsets);
     }
 }
