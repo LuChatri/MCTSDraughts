faecc9045e71704fbafef732c9f919dec0a7fbaa
CluelessMe <ldcmiga@gmail.com>
2021-06-24 12:10:47 -0600

    Write MCTSNode class

---
+++ src/main/java/MCTSNode.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a tree node containing information for Monte Carlo Tree Search.
+ */
+public class MCTSNode extends Node<MCTSNode, Move> {
+
+    private double value;
+    private int visits;
+
+    /**
+     * Constructs an MCTSNode.
+     *
+     * Note that children should be mutable (not from Arrays.asList or
+     * the like) if you plan to call methods like {@link Node#addChild(Node)}.
+     *
+     * Although value and visits typically represent wins and playouts, you are
+     * welcome to use them for something else. Thus, both accept negative values.
+     *
+     * @param value Number of wins in playouts from this node.
+     * @param visits Number of playouts from this node.
+     * @param parent Node one step higher in the tree.
+     * @param children Node one step lower in the tree.
+     * @param data The move associated with this node.
+     */
+    public MCTSNode(double value, int visits, MCTSNode parent, List<MCTSNode> children, Move data) {
+        super(parent, children, data);
+        setValue(value);
+        setVisits(visits);
+    }
+
+    /**
+     * Constructs a node with the specified parent and no children, data, value, or visits.
+     */
+    public MCTSNode(MCTSNode parent) {
+        this(0, 0, parent, new ArrayList<>(), null);
+    }
+
+    /**
+     * Constructs a root node with no children, data, value, or visits.
+     */
+    public MCTSNode() {
+        this(0, 0, null, new ArrayList<>(), null);
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public MCTSNode setValue(double value) {
+        this.value = value;
+        return getThis();
+    }
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public MCTSNode setVisits(int visits) {
+        this.visits = visits;
+        return getThis();
+    }
+
+    /**
+     * Must be implemented per the abstract class Node.
+     *
+     * @return This node
+     */
+    public MCTSNode getThis() {
+        return this;
+    }
+
+}
