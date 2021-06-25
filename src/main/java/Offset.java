140064253648e4f6b1c22aaae0c74874dbbc25cb
CluelessMe <ldcmiga@gmail.com>
2021-06-24 18:44:21 -0600

    Remove Offset offset attribute

--- src/main/java/Offset.java
+++ src/main/java/Offset.java
@@ -1,20 +1,10 @@
 public enum Offset {
-    MOVE_SOUTHEAST(5),
-    MOVE_SOUTHWEST(4),
-    MOVE_NORTHEAST(-MOVE_SOUTHWEST.offset),
-    MOVE_NORTHWEST(-MOVE_SOUTHEAST.offset),
-    JUMP_SOUTHEAST(9),
-    JUMP_SOUTHWEST(7),
-    JUMP_NORTHEAST(-JUMP_SOUTHWEST.offset),
-    JUMP_NORTHWEST(-JUMP_SOUTHEAST.offset);
-
-    private final int offset;
-
-    private Offset(int offset) {
-        this.offset = offset;
-    }
-
-    public int getOffset() {
-        return offset;
-    }
+    MOVE_SOUTHEAST,
+    MOVE_SOUTHWEST,
+    MOVE_NORTHEAST,
+    MOVE_NORTHWEST,
+    JUMP_SOUTHEAST,
+    JUMP_SOUTHWEST,
+    JUMP_NORTHEAST,
+    JUMP_NORTHWEST;
 }
